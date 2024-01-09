2803e85794cb86ada1b3c33825c91282d9c21602
Danny Nguyen <nguyencaodat27052004@gmail.com>
2024-01-09 14:46:09 -0700

    Week 2 fix styling

--- app/page.js
+++ app/page.js
@@ -3,8 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href='/week-2'>Week 2</Link>
+      <div className='flex justify-center flex-col px-[16rem] py-[6rem]'>
+        <h1 className='text-4xl font-bold'>
+          CPRG 306: Web Development 2 - Assignments
+        </h1>
+        <Link
+          href='/week-2'
+          className='text-lg mt-[1.25rem] hover:underline hover:text-green-400'
+        >
+          Week 2 Assignment
+        </Link>
+      </div>
     </main>
   );
 }
