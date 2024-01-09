83d2422917ec091fbc3693e65163df8c6e66fc15
Danny Nguyen <nguyencaodat27052004@gmail.com>
2024-01-09 14:29:41 -0700

    Week-2 upload

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Cao Dat Nguyen</h1>
+      <Link href='https://github.com/Danny2705?tab=repositories'>
+        https://github.com
+      </Link>
+    </main>
+  );
+}
